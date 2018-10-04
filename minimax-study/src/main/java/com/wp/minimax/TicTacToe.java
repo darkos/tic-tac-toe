8fb4d4dabf509156bf3a8f2a96b9bfdad2980edf
Darko Stefanovic <stefanovic.darko@gmail.com>
2018-10-03 23:43:17 -0500

    initia code commit

---
+++ minimax-study/src/main/java/com/wp/minimax/TicTacToe.java
@@ -0,0 +1,23 @@
+package com.wp.minimax;
+
+public class TicTacToe {
+    
+    public TicTacToe() {
+
+    }
+    int[][] emptyBoard = new int[3][3];
+    int[][] testBoard = {{0, -1, 1}, {1, -1, 1}, {-1, 0, 0}};
+    public void printNextMoves() {
+
+    }
+
+    public String getAsciiBoard(int[][] board) {
+        // TODO
+        return "";
+    }
+
+    public String getAsciiRow(int[] row) {
+        // TODO
+        return "";
+    } 
+}
